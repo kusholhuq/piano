81821f5d2095948c9d997e2f5fac25f32b03fb41
Mohsinul Huq <kusholhuq@gmail.com>
2020-10-25 02:28:45 -0700

    its working right now, about to delete server

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Test(props){
+  return(
+    <div>
+    <div>cool test 2</div>
+    <div>another cool test</div>
+    <div>another one</div>
+    </div>
+  )
+}
