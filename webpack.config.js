f4a224f1bde13b7a57e278490af31b43ae909ca8
Mohsinul Huq <kusholhuq@gmail.com>
2020-10-25 01:55:56 -0700

    trying it with the sgt react webpack and some additional packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
+const path = require('path');
+
+const srcPath = path.resolve(__dirname, 'client');
+const publicPath = path.resolve(__dirname, 'server/public');
+
 module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+  entry: './client',
+  output: {
+    path: publicPath
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
+        include: srcPath,
         use: {
           loader: 'babel-loader',
           options: {
@@ -16,5 +26,16 @@ module.exports = {
         }
       }
     ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    contentBase: publicPath,
+    watchContentBase: true,
+    stats: 'minimal',
+    proxy: {
+      '/api': 'http://localhost:3001'
+    }
   }
 };
